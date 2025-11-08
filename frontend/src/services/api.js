fef5db4c4a9a94de9809c3dee160a402d7931b23
MirajBalghari <mirajhussain542@gmail.com>
2025-11-09 00:29:15 +0500

    add backend URL in frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:'http://localhost:8000/api',
+    baseURL:'https://full-stack-blogs-application.vercel.app/api',
     withCredentials:true
 })
 
